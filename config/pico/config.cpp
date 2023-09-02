1793f8e0c262f16aadd9688e9c0f22927dd468b2
Cristian <125297328+ramirc5@users.noreply.github.com>
2023-09-02 15:10:14 -0600

    cw os off

--- config/pico/config.cpp
+++ config/pico/config.cpp
@@ -120,7 +120,7 @@ void setup() {
                 primary_backend, new B0XXInputViewer(input_sources, input_source_count)
             };
             primary_backend->SetGameMode(
-                new Melee20Button(socd::SOCD_2IP_NO_REAC, { .crouch_walk_os = true })
+                new Melee20Button(socd::SOCD_2IP_NO_REAC, { .crouch_walk_os = false })
                 //new FgcMode(socd::SOCD_NEUTRAL, socd::SOCD_NEUTRAL)
             );
         }
@@ -129,7 +129,7 @@ void setup() {
             primary_backend =
                 new GamecubeBackend(input_sources, input_source_count, pinout.joybus_data);
                 primary_backend->SetGameMode(
-                    new Melee20Button(socd::SOCD_2IP_NO_REAC, { .crouch_walk_os = true })
+                    new Melee20Button(socd::SOCD_2IP_NO_REAC, { .crouch_walk_os = false })
                     //new Ultimate(socd::SOCD_2IP)
                 );
         } else if (console == ConnectedConsole::N64) {
