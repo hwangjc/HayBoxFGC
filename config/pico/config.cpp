c2a71ea2ba514ac635bbcbe181d986c742121acd
Cristian <125297328+ramirc5@users.noreply.github.com>
2023-10-21 01:27:53 -0700

    new default

--- config/pico/config.cpp
+++ config/pico/config.cpp
@@ -120,8 +120,8 @@ void setup() {
                 primary_backend, new B0XXInputViewer(input_sources, input_source_count)
             };
             primary_backend->SetGameMode(
-                //new Melee20Button(socd::SOCD_2IP_NO_REAC, { .crouch_walk_os = false })
-                new FgcMode(socd::SOCD_NEUTRAL, socd::SOCD_NEUTRAL)
+                new Melee20Button(socd::SOCD_2IP_NO_REAC, { .crouch_walk_os = false })
+                //new FgcMode(socd::SOCD_NEUTRAL, socd::SOCD_NEUTRAL)
                 /*
                 new ProjectM(
                     socd::SOCD_2IP_NO_REAC,
@@ -135,8 +135,8 @@ void setup() {
             primary_backend =
                 new GamecubeBackend(input_sources, input_source_count, pinout.joybus_data);
                 primary_backend->SetGameMode(
-                    //new Melee20Button(socd::SOCD_NEUTRAL, { .crouch_walk_os = false })
-                    new Ultimate(socd::SOCD_2IP)
+                    new Melee20Button(socd::SOCD_NEUTRAL, { .crouch_walk_os = false })
+                    //new Ultimate(socd::SOCD_2IP)
                     /*
                     new ProjectM(
                         socd::SOCD_2IP_NO_REAC,
